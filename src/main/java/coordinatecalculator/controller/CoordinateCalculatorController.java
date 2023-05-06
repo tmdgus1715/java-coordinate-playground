70eb74a256da0fabb1626b55f83d1485d95b8606
tmdgus1715 <songseunghyeon@songseunghyeon-ui-MacBookPro.local>
2023-05-06 18:35:45 +0900

    feat: Controller추가

--- src/main/java/coordinatecalculator/controller/CoordinateCalculatorController.java
+++ src/main/java/coordinatecalculator/controller/CoordinateCalculatorController.java
@@ -1,2 +1,26 @@
-package coordinatecalculator.controller;public class CoordinateCalculatorController {
+package coordinatecalculator.controller;
+
+import coordinatecalculator.domain.Figure;
+import coordinatecalculator.domain.FigureGenerator;
+import coordinatecalculator.domain.Point;
+import coordinatecalculator.view.InputView;
+import coordinatecalculator.view.OutputView;
+
+import java.util.List;
+
+public class CoordinateCalculatorController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public CoordinateCalculatorController() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+    }
+
+    public void start() {
+        List<Point> points = inputView.input();
+        Figure figure = FigureGenerator.generate(points);
+        outputView.output(figure);
+    }
 }
